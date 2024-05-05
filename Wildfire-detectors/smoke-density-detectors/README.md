541db1d022381f7cb875db4db74f1495ba54caa7
NicolasIsla <nico.isla1@gmail.com>
2024-05-05 18:31:29 -0400

    init repo

---
+++ Wildfire-detectors/smoke-density-detectors/README.md
@@ -0,0 +1,12 @@
+## Nemo Wildfire smoke density detectors:
+
+#### Based on DETR:
+- [Nemo-DETR-dg](https://nevada.box.com/s/ad391d20lsjfh0n85z9pc3hj739462x3)
+
+
+#### Based on Faster RCNN:
+
+  - Nemo-FRCNN-dg: [.pth](https://nevada.box.com/s/tckoxw2pjq6brfhnlgsrq22lnarc7qwd), [config.yaml](https://nevada.box.com/s/vynwtiw8sgjkbxn0326m2qz2vy3nre5t), [readme](https://nevada.box.com/s/nddr6tlwlp2fn5z9yd4dj46enpnp5e3o)
+
+#### Based on RetinaNet:
+  - Nemo-RNet-dg: [.pth](https://nevada.box.com/s/dtdq1a49nvdo26vw6wjh4pxscorp1ezp), [config.yaml](https://nevada.box.com/s/bqq6ofrdop5r1f1ri9qufpdhpl2cw54y), [readme](https://nevada.box.com/s/nddr6tlwlp2fn5z9yd4dj46enpnp5e3o)
---
+++ download_data.py
@@ -0,0 +1,32 @@
+import gdown
+import zipfile
+import os
+import shutil
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def download_and_extract(url, destination):
+    gdown.download(f"https://drive.google.com/uc?id={url}", destination, quiet=False)
+
+    with zipfile.ZipFile(destination, 'r') as zip_ref:
+        zip_ref.extractall(os.path.dirname(destination))
+    os.remove(destination)
+
+if __name__ == "__main__":
+    path = os.getenv('PATH_DATASET')
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    file_urls = {
+        'train': os.getenv("FILE_URL1_ID"),
+        'val': os.getenv("FILE_URL2_ID"),
+    }
+
+    for dataset_id, url in file_urls.items():
+        destination = os.path.join(path, f"file_{dataset_id}.zip")
+        download_and_extract(url, destination)
+        print(f"Dataset {dataset_id} downloaded and extracted successfully.")
+
+   
+
