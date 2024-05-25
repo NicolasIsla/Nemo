9fde26ed5773b2db1e8df989076053412bfacfba
NicolasIsla <nico.isla1@gmail.com>
2024-05-25 09:52:25 -0400

    dsa

--- detr/engine.py
+++ detr/engine.py
@@ -160,7 +160,7 @@ def metrics(model, postprocessors, data_loader, base_ds, device, output_dir):
 
     iou_types = tuple(k for k in ('segm', 'bbox') if k in postprocessors.keys())
     coco_evaluator = CocoEvaluator(base_ds, iou_types)
-    coco_evaluator.coco_eval[iou_types[0]].params.iouThrs = [0, 0.1, 0.5, 0.75]
+    coco_evaluator.coco_eval[iou_types[0]].params.iouThrs = [0, 0.1, 0.5, 0.3]
 
 
     for samples, targets in metric_logger.log_every(data_loader, 10, header):
