5e6cabad69e9f1d752e476ee769231898f73a04f
NicolasIsla <nico.isla1@gmail.com>
2024-05-26 20:31:35 -0400

    dasd

--- detr/engine.py
+++ detr/engine.py
@@ -181,7 +181,7 @@ def metrics(model, postprocessors, data_loader, base_ds, device, output_dir):
         
         res = {target['image_id'].item(): output for target, output in zip(targets, results)}
         print(targets)
-        print(results)
+        print(outputs)
         break
         
         if coco_evaluator is not None:
